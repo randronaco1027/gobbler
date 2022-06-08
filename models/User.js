18a21e7a7c6dc5f4502eebe29d5e68a69677ff9f
randronaco <randronaco1027@gmail.com>
2022-06-07 22:17:36 -0400

    fixed user model

--- models/User.js
+++ models/User.js
@@ -21,6 +21,10 @@ User.init(
             allowNull: false,
             unique: true,
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         password: {
             type: DataTypes.STRING,
             allowNull: false
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,18 +3,22 @@ const { User } = require('../models')
 const userData = [
     {
         username: 'Betty Crocker',
+        email: 'crocker12@aol.com',
         password: 'thecrockster123'
     },
     {
         username: 'Colonel Sanders',
+        email: 'kfc1952@yahoo.com',
         password: 'fingerlickinggood'
     },
     {
         username: 'Julia Child',
+        email: 'jchild1@gmail.com',
         password: 'mastertheart'
     },
     {
         username: 'Chef Boyardee',
+        email: 'pastaman@aol.com',
         password: 'spaghettios'
     }
 ]
